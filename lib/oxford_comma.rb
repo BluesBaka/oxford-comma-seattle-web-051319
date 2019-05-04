949a6c97c96e9275fa4d42f0b5f0e194094392a8
Bryna Dunnells <badunnells@gmail.com>
2019-05-04 04:04:23 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,5 +5,7 @@ def oxford_comma(array)
     if array.length == 2
       return array.join(" and ")
     end
+    if array.length == 3
+      return "#{array[0]}, #{array[1]}, and #{array[2]}"
 
 end
